50205f5a5d98c00f6d9bf3cee943d740cdbd6202
Hardik <dev.hardik200@gmail.com>
2021-09-20 20:19:29 +0530

    done

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ async function success(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
 
-  const location = await (await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
-  )).json();
+  const location = await (
+    await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+    )
+  ).json();
   console.log("location:", location);
+  const taluka = location.localityInfo.administrative[3].name;
+  const district = location.localityInfo.administrative[2].name;
+  const state = location.localityInfo.administrative[1].name;
+  const country = location.localityInfo.administrative[0].name;
+
+  console.log(taluka + " " + district + " " + state + " " + country);
 }
 function error(error) {
   console.log(error);
